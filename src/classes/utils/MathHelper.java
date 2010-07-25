174df26e5070fe07bf17970ed5600f490345784c
Gábor Pápai <gabor.papai@gmail.com>
2010-07-25 18:16:05 +0000

    fix mathhelper

--- src/classes/utils/MathHelper.java
+++ src/classes/utils/MathHelper.java
@@ -1,33 +1,40 @@
 package classes.utils;
 
+import java.util.Random;
+
 public class MathHelper {
 
 	private static final float HALF = 0.5f;
+	private static Random      random;
 
 	public static int randomInt(int max) {
-		return (int)Math.floor(Math.random()*( max + 1));
+		return (int) Math.floor(random.nextDouble() * (max + 1));
 	}
 
 	public static int randomInt(int min, int max) {
-		return (int)Math.floor(Math.random()*(max - min + 1)) + min;
+		return (int) Math.floor(random.nextDouble() * (max - min + 1)) + min;
 	}
-	
+
 	public static boolean randomBoolean() {
-		return Math.random() < 0.5f;
+		return random.nextBoolean();
 	}
-	
+
 	public static boolean checkRandomEvent(float possibility) {
-		return Math.random() < possibility;
+		return random.nextFloat() < possibility;
 	}
 
 	public static double halfHasMoreChancePossibility() {
 		// power: result has more chance to be near of 0 as near of 0.5;
-		double dif = Math.pow(Math.random(), 3) / 2; 
+		double dif = Math.pow(random.nextDouble(), 3) / 2;
 		if (randomBoolean()) {
 			dif = -1 * dif;
 		}
-		
+
 		return HALF + dif;
+	}
+
+	public static void setRandom(Random random) {
+		MathHelper.random = random;
 	}
 
 }
