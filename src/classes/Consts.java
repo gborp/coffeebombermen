9e84b2ad7ff5abbd69315607d563b7ed41f8b4ee
Gábor Pápai <gabor.papai@gmail.com>
2010-08-03 18:07:30 +0000

    server name and version

--- src/classes/Consts.java
+++ src/classes/Consts.java
@@ -1,4 +1,3 @@
-
 /*
  * Created on July 7, 2004
  */
@@ -6,29 +5,29 @@
 package classes;
 
 /**
- * Holds constants for the game describing it as a software
- * and about the directory hierarchy and file informations
- *
+ * Holds constants for the game describing it as a software and about the
+ * directory hierarchy and file informations
+ * 
  * @author Andras Belicza
  */
 public class Consts {
 
-    /** Name of the application.    */
-    public static final String APPLICATION_NAME        = "Bombermen";
-    /** Version of the application. */
-    public static final String APPLICATION_VERSION     = "0.69";
-    
-    /** Name of the directory containing the levels.                             */
-    public static final String LEVELS_DIRECTORY_NAME   = "levels/";
-    /** Name of the directory containing the graphic files (the graphic themes). */
-    public static final String GRAPHICS_DIRECTORY_NAME = "graphics/";
-    /** Name of the directory containing the sounds (the sound themes).          */
-    public static final String SOUND_DIRECTORY_NAME    = "sound/";
+	/** Name of the application. */
+	public static final String APPLICATION_NAME        = "Coffee Bombermen";
+	/** Version of the application. */
+	public static final String APPLICATION_VERSION     = "1.5";
 
-    /** Extension of the level files .                                           */
-    public static final String LEVEL_FILE_EXTENSION    = ".bole";
+	/** Name of the directory containing the levels. */
+	public static final String LEVELS_DIRECTORY_NAME   = "levels/";
+	/** Name of the directory containing the graphic files (the graphic themes). */
+	public static final String GRAPHICS_DIRECTORY_NAME = "graphics/";
+	/** Name of the directory containing the sounds (the sound themes). */
+	public static final String SOUND_DIRECTORY_NAME    = "sound/";
 
-    /** Name of the file containing the (permanent) options.                     */
-    public static final String OPTIONS_FILE_NAME       = "options.txt";
-    
+	/** Extension of the level files . */
+	public static final String LEVEL_FILE_EXTENSION    = ".bole";
+
+	/** Name of the file containing the (permanent) options. */
+	public static final String OPTIONS_FILE_NAME       = "options.txt";
+
 }
