dc8bb374e49352f178a500e13d2fa3be37115870
Gábor Pápai <gabor.papai@gmail.com>
2010-12-19 19:22:08 +0000

    Robot: REVERSE disease

--- src/com/braids/coffeebombermen/client/gamecore/robot/IRobot.java
+++ src/com/braids/coffeebombermen/client/gamecore/robot/IRobot.java
@@ -1,12 +1,9 @@
 package com.braids.coffeebombermen.client.gamecore.robot;
 
-import com.braids.coffeebombermen.client.gamecore.robot.SimpleRobot.AStarNode;
 
 public interface IRobot {
 
 	String getNextAction();
-
-	public AStarNode[][] getLastNodes();
 
 	void initForNextRound();
 
